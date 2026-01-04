fd7aad5bdc6219aeb0b1a4c4d1def8eeffd1a2b0
Mark <111501559+Praydchicken@users.noreply.github.com>
2026-01-04 19:39:32 +0800

    feat: add number input handling

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log('Hello World');
+const buttonsElement = document.querySelector('.js-buttons');
+
+let currentInput = '';
+
+buttonsElement.addEventListener('click', (event) => {
+	const button = event.target.closest('button');
+
+	if (!button) {
+		return;
+	}
+
+	const val = button.textContent;
+
+	if (!isNaN(Number(val)) || val === '.') {
+		handleNumbers(val);
+	}
+});
+
+function handleNumbers(number) {
+	if (number === '.' && currentInput.includes('.')) {
+		console.log('Already have decimals');
+		return;
+	}
+
+	currentInput += number;
+	console.log(`current input: ${currentInput}`);
+}
